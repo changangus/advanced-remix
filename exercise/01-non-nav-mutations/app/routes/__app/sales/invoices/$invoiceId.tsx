34c5dbe6c1ba8655a58323a979d206d703f643a0
Angus Chang <changangus2@gmail.com>
2023-03-14 13:20:05 -0400

    feat: exercise one

--- exercise/01-non-nav-mutations/app/routes/__app/sales/invoices/$invoiceId.tsx
+++ exercise/01-non-nav-mutations/app/routes/__app/sales/invoices/$invoiceId.tsx
@@ -172,7 +172,6 @@ function Deposits() {
       ) : (
         <div>None yet</div>
       )}
-      {/* 🐨 change this to your fetcher.Form */}
       <fetcher.Form
         method="post"
         className="grid grid-cols-1 gap-x-4 gap-y-2 lg:grid-cols-2"
